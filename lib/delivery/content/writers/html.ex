3edae5231239cfbed23f49e365e1068bebe0b75c
Darren Siegel <siegel.darren@gmail.com>
2019-11-04 07:41:14 -0500

    html

---
+++ lib/delivery/content/writers/html.ex
@@ -0,0 +1,192 @@
+defmodule Delivery.Content.Writers.HTML do
+  alias Delivery.Content.Writers.Writer
+  alias Delivery.Content.Writers.Context
+
+  @behaviour Writer
+
+  def p(%Context{} = _context, next, _) do
+    ["<p>", next.(), "</p>\n"]
+  end
+
+  def table(%Context{} = _context, next, _) do
+    ["<table>", next.(), "</table>\n"]
+  end
+
+  def caption(%Context{} = _context, next, _) do
+    ["<caption>", next.(), "</caption>\n"]
+  end
+
+  def thead(%Context{} = _context, next, _) do
+    ["<thead>", next.(), "</thead>\n"]
+  end
+
+  def tbody(%Context{} = _context, next, _) do
+    ["<tbody>", next.(), "</tbody>\n"]
+  end
+
+  def tfoot(%Context{} = _context, next, _) do
+    ["<tfoot>", next.(), "</tfoot>\n"]
+  end
+
+  def tr(%Context{} = _context, next, _) do
+    ["<tr>", next.(), "</tr>\n"]
+  end
+
+  def td(%Context{} = _context, next, _) do
+    ["<td>", next.(), "</td>\n"]
+  end
+
+  def th(%Context{} = _context, next, _) do
+    ["<th>", next.(), "</th>\n"]
+  end
+
+  def image(%Context{} = _context, _, %{data: %{"src" => src}}) do
+    ["<image src=\"", src, "\"/>\n"]
+  end
+
+  def youtube(%Context{} = _context, _, %{data: %{"src" => src}}) do
+    ["<youtube src=\"", src, "\"/>\n"]
+  end
+
+  def ul(%Context{} = _context, next, _) do
+    ["<ul>", next.(), "</ul>\n"]
+  end
+
+  def ol(%Context{} = _context, next, _) do
+    ["<ol>", next.(), "</ol>\n"]
+  end
+
+  def li(%Context{} = _context, next, _) do
+    ["<li>", next.(), "</li>\n"]
+  end
+
+  def header(%Context{} = _context, next) do
+    ["<section><title>", next.(), "</title><body><p></p></body></section>\n"]
+  end
+
+  def h1(%Context{} = context, next, _) do
+    header(context, next)
+  end
+
+  def h2(%Context{} = context, next, _) do
+    header(context, next)
+  end
+
+  def h3(%Context{} = context, next, _) do
+    header(context, next)
+  end
+
+  def h4(%Context{} = context, next, _) do
+    header(context, next)
+  end
+
+  def h5(%Context{} = context, next, _) do
+    header(context, next)
+  end
+
+  def h6(%Context{} = context, next, _) do
+    header(context, next)
+  end
+
+  def audio(%Context{} = _context, _, %{data: %{"src" => src}}) do
+    ["<audio src=\"", src, "\"/>\n"]
+  end
+
+  def blockquote(%Context{} = _context, next, _) do
+    ["<quote>", next.(), "</quote>\n"]
+  end
+
+  def codeblock(%Context{} = _context, next, %{data: %{"syntax" => syntax}}) do
+    ["<codeblock syntax=\"#{syntax}\">", next.(), "</codeblock>\n"]
+  end
+
+  def codeline(%Context{} = _context, _, %{nodes: nodes}) do
+    Enum.map(nodes, fn n -> n.text end)
+  end
+
+  def a(%Context{} = _context, next, %{data: %{"href" => href}}) do
+    ["<link href=\"#{escape_xml(href)}\">", next.(), "</link>\n"]
+  end
+
+  def definition(%Context{} = _context, next, _) do
+    ["<extra>", next.(), "</extra>\n"]
+  end
+
+  def text(%Context{} = _context, %{text: text, marks: marks}) do
+    escape_xml(text) |> wrap_marks(marks)
+  end
+
+  def escape_xml(text) do
+    String.replace(text, "&", "&amp;")
+    |> String.replace("<", "&lt;")
+    |> String.replace(">", "&gt;")
+    |> String.replace("'", "&apos;")
+    |> String.replace("\"", "&quot;")
+  end
+
+  def document(%Context{} = _context, next, %{data: %{id: id, title: title}}) do
+    [
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n",
+      "<!DOCTYPE workbook_page PUBLIC \"-//Carnegie Mellon University//DTD Workbook Page MathML 3.8//EN\" \"http://oli.web.cmu.edu/dtd/oli_workbook_page_mathml_3_8.dtd\">\n",
+      "<workbook_page id=\"#{id}\"><head><title>#{escape_xml(title)}</title></head><body>\n",
+      next.(),
+      "</body></workbook_page>"
+    ]
+  end
+
+  def organization(%Context{} = _context, next, %{id: id, title: title}) do
+    [
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n",
+      "<!DOCTYPE organization PUBLIC \"-//Carnegie Mellon University//DTD Content Organization Simple 2.3//EN\" \"http://oli.web.cmu.edu/dtd/oli_content_organization_simple_2_3.dtd\">\n",
+      "<organization id=\"#{id}\" version=\"1.0\">\n",
+      "<title>#{escape_xml(title)}</title>\n",
+      "<description>organization description</description>\n",
+      "<audience>Audience</audience>\n",
+      "<labels sequence=\"Sequence\" unit=\"Unit\" module=\"Module\" section=\"Section\"/>\n",
+      "<sequences>\n",
+      "<sequence id=\"main_sequence\" category=\"content\" audience=\"all\">\n",
+      "<title>Main Sequence</title>\n",
+      next.(),
+      "</sequence>\n",
+      "</sequences></organization>\n"
+    ]
+  end
+
+  def module(%Context{} = _context, next, %{id: id, title: title}) do
+    [
+      "<module id=\"#{id}\">\n",
+      "<title>#{escape_xml(title)}</title>\n",
+      next.(),
+      "</module>\n"
+    ]
+  end
+
+  def reference(%Context{} = _context, _, %{id: id}) do
+    [
+      "<item id=\"ref_#{id}\" scoring_mode=\"default\">\n",
+      "<resourceref idref=\"#{id}\"/>\n",
+      "</item>\n"
+    ]
+  end
+
+  def wrap_marks(text, marks) do
+    map = %{
+      "sub" => "sub",
+      "sup" => "sup",
+      "bold" => "em",
+      "underline" => "",
+      "code" => "code",
+      "italic" => "em",
+      "strikethrough" => "em",
+      "mark" => "em"
+    }
+
+    Enum.reverse(marks)
+    |> Enum.reduce(
+      text,
+      fn %{type: m}, t ->
+        "<" <> Map.get(map, m) <> ">" <> t <> "</" <> Map.get(map, m) <> ">"
+      end
+    )
+  end
+end
